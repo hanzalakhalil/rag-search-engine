d3a14d469d2ee1e77b12ab74dc74809b3f9f0edb
hanzalakhalil <hanzalakhalilpk@gmail.com>
2026-08-24 23:50:07 +0500

    semantic chunk

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,6 +1,7 @@
 import argparse
 import lib.semantic_search as ss
 import util.helpers as helpers
+import re
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -23,7 +24,12 @@ def main() -> None:
     chunk_parser = subparsers.add_parser("chunk", help="for chunking")
     chunk_parser.add_argument("text", type=str, help="the text to make chunks out of")
     chunk_parser.add_argument("--chunk-size", default=200, type=int, help="number of words per chunk")
-    chunk_parser.add_argument("--overlap", type=int, help="overlap between chunks")
+    chunk_parser.add_argument("--overlap",default=0, type=int, help="overlap between chunks")
+    
+    semantic_chunk_parser = subparsers.add_parser("semantic_chunk", help="for semantic chunking")
+    semantic_chunk_parser.add_argument("text", type=str, help="the text to make chunks out of")
+    semantic_chunk_parser.add_argument("--max-chunk-size", default=4, type=int, help="number of words per chunk")
+    semantic_chunk_parser.add_argument("--overlap", type=int,default=0, help="overlap between chunks")
     
     args = parser.parse_args()
     
@@ -42,13 +48,15 @@ def main() -> None:
             search_command(args.query,int(args.limit))
         case "chunk":
             chunk_command(args.text,args.chunk_size,args.overlap)
+        case "semantic_chunk":
+            semantic_chunk_command(args.text,args.max_chunk_size,args.overlap)
         case _:
             parser.print_help()
 
 def search_command(query: str, limit: int) -> None:
     search_instance = ss.SemanticSearch()
     documents = helpers.load_movies()["movies"]
-    embeddings = search_instance.load_or_create_embeddings(documents)
+    search_instance.load_or_create_embeddings(documents)
     results = search_instance.search(query,limit)
     i=1
     for result in results:
@@ -79,5 +87,26 @@ def chunk_command(text:str , chunk_size : int, overlap: int):
             j+=1
         print(f"{i+1}. {temp}")
         i+=1
+        
+def semantic_chunk_command(text:str , max_chunk_size : int, overlap: int):
+    text_list = re.split(r"(?<=[.!?])\s+",text) 
+    chunk_list=[]
+    for i in range(0,len(text_list),max_chunk_size-overlap):
+        
+        chunk_list.append(text_list[i:i+max_chunk_size])
+        if(i+max_chunk_size >= len(text_list)):
+            break
+            
+    print(f"Semantically chunking {len(text)} characters")
+    i=0
+    for chunk in chunk_list:
+        j=0
+        temp:str=""
+        for word in chunk:
+            temp = temp + word+" "
+            j+=1
+        print(f"{i+1}. {temp}")
+        i+=1
+
 if __name__ == "__main__":
     main()
