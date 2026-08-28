bfa5ded2f82631c7064e673f8e24c502a569e50a
hanzalakhalil <hanzalakhalilpk@gmail.com>
2026-08-29 03:42:30 +0500

    semantic_chunk

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -2,6 +2,9 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 import os
 import util.helpers as helpers
+import json
+import re
+
 
 class SemanticSearch:
     def __init__(self, model_name: str = "all-MiniLM-L6-v2"):
@@ -58,7 +61,54 @@ class SemanticSearch:
             raise ValueError("The query is empty")
         embedding = self.model.encode([text])
         return embedding[0]
+  
+class ChunkedSemanticSearch(SemanticSearch):
+    def __init__(self, model_name: str = "all-MiniLM-L6-v2") -> None:
+        super().__init__(model_name)
+        self.chunk_embeddings = None
+        self.chunk_metadata = None
     
+    def build_chunk_embeddings(self, documents: list[dict]) -> np.ndarray:
+        self.documents = documents
+        chunk_list:list[str]=[]
+        metadata:list[dict]=[]
+        for movie_idx,doc in enumerate(self.documents):
+            self.document_map[movie_idx]=doc
+        temp_list:list[str]=[]
+        for movie_idx,doc in enumerate(self.documents):
+            if doc["description"]=="":
+                continue
+            for chunk in semantic_chunk_command(doc["description"],4,1):
+                temp=""
+                for sen in chunk:
+                    temp= temp+ " " + sen 
+                chunk_list.append(temp)
+                temp_list.append(temp)
+            for chunk_idx,chunk_text in enumerate(temp_list):
+                metadata.append({"movie_idx":movie_idx,"chunk_idx":chunk_idx,"total_chunks":len(temp_list)})
+            temp_list=[]
+
+        
+        self.chunk_metadata = metadata
+        self.chunk_embeddings = self.model.encode(chunk_list,show_progress_bar=True)
+        np.save("cache/chunk_embeddings.npy", self.chunk_embeddings)
+        with open("cache/chunk_metadata.json", "w") as f:
+            json.dump({"chunks": self.chunk_metadata, "total_chunks": len(chunk_list)}, f, indent=2)    
+        return self.chunk_embeddings
+    
+    def load_or_create_chunk_embeddings(self, documents: list[dict]) -> np.ndarray:
+        self.documents = documents
+        for movie_idx,doc in enumerate(self.documents):
+            self.document_map[movie_idx]=doc
+        if os.path.exists("cache/chunk_embeddings.npy") and os.path.exists("cache/chunk_metadata.json"):
+            self.chunk_embeddings=np.load("cache/chunk_embeddings.npy",)
+            with open("cache/chunk_metadata.json", "r") as f:
+                data = json.load(f)
+            self.chunk_metadata = data["chunks"]
+            return self.chunk_embeddings
+        return self.build_chunk_embeddings(self.documents)
+
+  
 def verify_embeddings() -> None:
     search = SemanticSearch()
     documents = helpers.load_movies()["movies"]
@@ -95,3 +145,55 @@ def cosine_similarity(vec1: np.ndarray, vec2: np.ndarray) -> float:
         return 0.0
 
     return dot_product / (norm1 * norm2)
+
+def search_command(query: str, limit: int) -> None:
+    search_instance = SemanticSearch()
+    documents = helpers.load_movies()["movies"]
+    search_instance.load_or_create_embeddings(documents)
+    results = search_instance.search(query,limit)
+    i=1
+    for result in results:
+        print(f'{i}. {result["title"]} {result["score"]}') 
+        print(f'{result["description"]:.80} ...')
+        i+=1
+    
+def chunk_command(text:str , chunk_size : int, overlap: int):
+    text_list = text.split()
+    chunk_list=[]
+    for i in range(0,len(text_list),chunk_size):
+        if(i-overlap+chunk_size < len(text_list)):
+            if(i-overlap<0):
+                chunk_list.append(text_list[i:i+chunk_size])
+            else:
+                chunk_list.append(text_list[i-overlap:i-overlap+chunk_size])
+                
+        else:
+            chunk_list.append(text_list[i-overlap:])
+            
+    print(f"Chunking {len(text)} characters")
+    i=0 
+    for chunk in chunk_list:
+        j=0
+        temp:str=""
+        for word in chunk:
+            temp = temp + word+" "
+            j+=1
+        print(f"{i+1}. {temp}")
+        i+=1
+        
+def semantic_chunk_command(text:str , max_chunk_size : int, overlap: int) -> list:
+    text_list = re.split(r"(?<=[.!?])\s+",text) 
+    chunk_list=[]
+    for i in range(0,len(text_list),max_chunk_size-overlap):
+        
+        chunk_list.append(text_list[i:i+max_chunk_size])
+        if(i+max_chunk_size >= len(text_list)):
+            break
+    return chunk_list
+
+def embed_chunks():
+    chunked_semantic_search = ChunkedSemanticSearch()
+    documents = helpers.load_movies()["movies"]
+    embeddings = chunked_semantic_search.load_or_create_chunk_embeddings(documents)
+    print(f"Generated {len(embeddings)} chunked embeddings")
+    
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,7 +1,6 @@
 import argparse
 import lib.semantic_search as ss
 import util.helpers as helpers
-import re
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -31,6 +30,8 @@ def main() -> None:
     semantic_chunk_parser.add_argument("--max-chunk-size", default=4, type=int, help="number of words per chunk")
     semantic_chunk_parser.add_argument("--overlap", type=int,default=0, help="overlap between chunks")
     
+    embed_chunks_parser = subparsers.add_parser("embed_chunks", help="for embedding chunks")
+    
     args = parser.parse_args()
     
     
@@ -45,68 +46,16 @@ def main() -> None:
         case "embed_query":
             ss.embed_query_text(args.query)
         case "search":
-            search_command(args.query,int(args.limit))
+            ss.search_command(args.query,int(args.limit))
         case "chunk":
-            chunk_command(args.text,args.chunk_size,args.overlap)
+            ss.chunk_command(args.text,args.chunk_size,args.overlap)
         case "semantic_chunk":
-            semantic_chunk_command(args.text,args.max_chunk_size,args.overlap)
+            ss.semantic_chunk_command(args.text,args.max_chunk_size,args.overlap)
+        case "embed_chunks":
+            ss.embed_chunks()
         case _:
             parser.print_help()
 
-def search_command(query: str, limit: int) -> None:
-    search_instance = ss.SemanticSearch()
-    documents = helpers.load_movies()["movies"]
-    search_instance.load_or_create_embeddings(documents)
-    results = search_instance.search(query,limit)
-    i=1
-    for result in results:
-        print(f'{i}. {result["title"]} {result["score"]}') 
-        print(f'{result["description"]:.80} ...')
-        i+=1
-    
-def chunk_command(text:str , chunk_size : int, overlap: int):
-    text_list = text.split()
-    chunk_list=[]
-    for i in range(0,len(text_list),chunk_size):
-        if(i-overlap+chunk_size < len(text_list)):
-            if(i-overlap<0):
-                chunk_list.append(text_list[i:i+chunk_size])
-            else:
-                chunk_list.append(text_list[i-overlap:i-overlap+chunk_size])
-                
-        else:
-            chunk_list.append(text_list[i-overlap:])
-            
-    print(f"Chunking {len(text)} characters")
-    i=0 
-    for chunk in chunk_list:
-        j=0
-        temp:str=""
-        for word in chunk:
-            temp = temp + word+" "
-            j+=1
-        print(f"{i+1}. {temp}")
-        i+=1
-        
-def semantic_chunk_command(text:str , max_chunk_size : int, overlap: int):
-    text_list = re.split(r"(?<=[.!?])\s+",text) 
-    chunk_list=[]
-    for i in range(0,len(text_list),max_chunk_size-overlap):
-        
-        chunk_list.append(text_list[i:i+max_chunk_size])
-        if(i+max_chunk_size >= len(text_list)):
-            break
-            
-    print(f"Semantically chunking {len(text)} characters")
-    i=0
-    for chunk in chunk_list:
-        j=0
-        temp:str=""
-        for word in chunk:
-            temp = temp + word+" "
-            j+=1
-        print(f"{i+1}. {temp}")
-        i+=1
 
 if __name__ == "__main__":
     main()
